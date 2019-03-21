d0d07a6a8241d710922cf5b7c742f20af08147a0
cep <terrtt>
2019-03-21 13:42:52 +0100

    Fragments Equipos

--- app/src/main/java/com/example/cep/entiespaisandroid/fragments/EquipsFragment.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/fragments/EquipsFragment.java
@@ -1,33 +1,29 @@
 package com.example.cep.entiespaisandroid.fragments;
 
 import android.app.Activity;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cep.entiespaisandroid.R;
 import com.example.cep.entiespaisandroid.classes.EQUIPS;
-import com.example.cep.entiespaisandroid.utilities.Conexions;
 
 import java.util.ArrayList;
 
 public class EquipsFragment extends Fragment
 {
-	private ListView llista_equips;
-	private Button afegir, editar, eliminar;
-	private EquipsListener listener;
-
+	private RecyclerView mRecyClerView;
+	private RecyclerView.Adapter mAdapter;
+	private RecyclerView.LayoutManager mLayoutManager;
 	private ArrayList<EQUIPS> equips = new ArrayList<EQUIPS>();
 
 	@Override
@@ -41,97 +37,106 @@ public class EquipsFragment extends Fragment
 	@Override
 	public void onActivityCreated(Bundle state) {
 		super.onActivityCreated(state);
-		equips.add(new EQUIPS(1, "F.C. Sant Cugat \nLorem ipsum", false, 1,
-				"2018-2019", 15, 3, 2, 1,
-				1));
-		equips.add(new EQUIPS(3, "F.C. Sant Cugat B \nLorem ipsum", false, 1,
-				"2018-2019", 15, 3, 2, 1,
-				1));
-		equips.add(new EQUIPS(4, "F.C. Sant Cugat Basquet \nLorem ipsum", false, 1,
-				"2018-2019", 15, 3, 2, 1,
-				1));
+		equips.add(new EQUIPS(1, "F.C. Sant Cugat", false,
+				1,"2018-2019", 15, 3,
+				2, 1,1));
+		equips.add(new EQUIPS(3, "F.C. Sant Cugat B", false,
+				1,"2018-2019", 15, 3,
+				2, 1,1));
+		equips.add(new EQUIPS(4, "F.C. Sant Cugat Basquet",
+				false, 1,"2018-2019", 15,
+				3, 2, 1,1));
 
-		llista_equips = getView().findViewById(R.id.ListViewEquips);
 
-		int[] colors = {0xFFFFFFFF, 0xFF43A047, 0xFFFFFFFF};
-		// https://convertingcolors.com/hex-color-FFFFFF.html
-		llista_equips.setDivider(new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors));
-		llista_equips.setDividerHeight(3);
-
-		llista_equips.setAdapter(new AdaptadorEquips(this));
-		llista_equips.setOnItemClickListener(new AdapterView.OnItemClickListener()
-		{
+		mRecyClerView = getView().findViewById(R.id.rv_recycler_view);
+		mRecyClerView.setHasFixedSize(true);
+		mLayoutManager = new GridLayoutManager(getContext(),
+				1, GridLayoutManager.VERTICAL, true);
+		mAdapter = new AdaptadorEquips(equips);
+		((AdaptadorEquips) mAdapter).setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-			{
-				if (listener != null)
-				{
-					listener.onEquipSeleccionado((EQUIPS)llista_equips.getAdapter().getItem(i));
-				}
+			public void onClick(View v) {
+				Toast.makeText(getContext(), "CLICK", Toast.LENGTH_LONG).show();
+
 			}
 		});
 
-		//---------------------------------------------------//
-		afegir = getView().findViewById(R.id.btnAfegirEquip);
-		afegir.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				Toast.makeText(getActivity(), "Añadir", Toast.LENGTH_LONG).show();
-			}
-		});
+		mRecyClerView.setLayoutManager(mLayoutManager);
+		mRecyClerView.setAdapter(mAdapter);
 
-		//---------------------------------------------------//
-		editar = getView().findViewById(R.id.btnEditarEquip);
-		editar.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				Toast.makeText(getActivity(), "Editar", Toast.LENGTH_LONG).show();
-			}
-		});
-
-		//---------------------------------------------------//
-		eliminar = getView().findViewById(R.id.btnEliminarEquip);
-		eliminar.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				Toast.makeText(getActivity(), "Eliminar", Toast.LENGTH_LONG).show();
-			}
-		});
-
-		//---------------------------------------------------//
 	}
 
-	public interface EquipsListener {
-		void onEquipSeleccionado(EQUIPS e);
-	}
 
-	public void setEquipsListener(EquipsListener listener) {
-		this.listener=listener;
-	}
 
-	class AdaptadorEquips extends ArrayAdapter<EQUIPS>
+	class AdaptadorEquips extends RecyclerView.Adapter <AdaptadorEquips.EquipViewHolder>
+			implements View.OnClickListener
 	{
-		Activity context;
+		private View.OnClickListener listener;
+		ArrayList<EQUIPS> equipos;
 
-		AdaptadorEquips(Fragment context) {
-			super(context.getActivity(), R.layout.items_listequips, /*Conexions.equips*/ equips);
-			this.context = context.getActivity();
+
+		public AdaptadorEquips(ArrayList<EQUIPS> lista_equipos) {
+			this.equipos = lista_equipos;
+		}
+		class EquipViewHolder
+				extends RecyclerView.ViewHolder
+		{
+			private ImageView imgEquipImage;
+			private TextView lblEquipName;
+			private TextView lblEquipEsport;
+
+
+			public EquipViewHolder(View itemView)
+			{
+				super(itemView);
+				imgEquipImage = itemView.findViewById(R.id.iv_image);
+				lblEquipName = itemView.findViewById(R.id.tv_text);
+				lblEquipEsport = itemView.findViewById(R.id.tv_blah);
+			}
+
+			public void bindEquip(EQUIPS e) {
+				imgEquipImage.setImageResource(R.drawable.ic_action_equips);
+				lblEquipName.setText(e.getNom());
+				lblEquipEsport.setText("Futbol");
+			}
 		}
 
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LayoutInflater inflater = context.getLayoutInflater();
-			View item = inflater.inflate(R.layout.items_listequips, null);
+		@NonNull
+		@Override
+		public EquipViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+		{
+			View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_item, viewGroup, false);
 
-			TextView IdTitulo = (TextView)item.findViewById(R.id.TextEquip);
-			IdTitulo.setText(/*Conexions.equips.get(position).getNom()*/equips.get(position).getNom());
+			itemView.setOnClickListener(this);
 
-			return(item);
+			EquipViewHolder evh = new EquipViewHolder(itemView);
+			return evh;
+		}
+		@Override
+		public void onBindViewHolder(@NonNull EquipViewHolder equipViewHolder, int i)
+		{
+			EQUIPS item = equipos.get(i);
+			equipViewHolder.bindEquip(item);
+		}
+
+		@Override
+		public int getItemCount()
+		{
+			return equipos.size();
+		}
+
+		public void setOnClickListener(View.OnClickListener listener) {
+			this.listener = listener;
+		}
+
+		@Override
+		public void onClick(View view) {
+			if(listener != null) {
+				listener.onClick(view);
+
+			}
+
+
 		}
 	}
 
