3a2b24569f350cf1b10ceedbe576c78e488bcd86
David <davidcep2018@gmail.com>
2019-03-25 23:22:50 +0100

    INSTALACIONES

--- app/src/main/java/com/example/cep/entiespaisandroid/fragments/InstalacionsFragment.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/fragments/InstalacionsFragment.java
@@ -1,7 +1,39 @@
 package com.example.cep.entiespaisandroid.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cep.entiespaisandroid.R;
+import com.example.cep.entiespaisandroid.classes.INSTALACIONS;
+
+import java.util.ArrayList;
 
 public class InstalacionsFragment extends Fragment
 {
+	private RecyclerView recyclerView;
+	private ArrayList<INSTALACIONS> instalacions;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+							 ViewGroup container,
+							 Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.fragment_instalacions, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle state) {
+		super.onActivityCreated(state);
+
+		instalacions = new ArrayList<>();
+		recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerInstalacions);
+		recyclerView.setHasFixedSize(true);
+
+		final AdapterInstalaciones adaptador = new AdapterInstalaciones(instalacions);
+		recyclerView.setAdapter(adaptador);
+	}
 }
