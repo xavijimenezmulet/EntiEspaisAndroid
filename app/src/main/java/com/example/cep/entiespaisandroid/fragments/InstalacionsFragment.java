2cedf35f70ab07a03dcb46a0a773a52415ccbfbf
David <davidcep2018@gmail.com>
2019-03-27 13:53:58 +0100

    INSTALACIÓN Y MAPA 2

--- app/src/main/java/com/example/cep/entiespaisandroid/fragments/InstalacionsFragment.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/fragments/InstalacionsFragment.java
@@ -1,7 +1,9 @@
 package com.example.cep.entiespaisandroid.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,10 +32,25 @@ public class InstalacionsFragment extends Fragment
 		super.onActivityCreated(state);
 
 		instalacions = new ArrayList<>();
+		instalacions.add(new INSTALACIONS(1, "Sabadell Poliesporiu", "pepe", "C/Lepanto 150",
+				"Externo", "sabadellpoli@gmail.com", "\\lelel.png", 150, 145));
 		recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerInstalacions);
 		recyclerView.setHasFixedSize(true);
 
 		final AdapterInstalaciones adaptador = new AdapterInstalaciones(instalacions);
+		recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 		recyclerView.setAdapter(adaptador);
+
+		adaptador.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				INSTALACIONS i = instalacions.get(recyclerView.getChildAdapterPosition(v));
+				Intent intent = new Intent(getContext(), FragmentInstalacio.class);
+				//intent.putExtra("instalacio", i);
+				startActivity(intent);
+			}
+		});
 	}
 }
