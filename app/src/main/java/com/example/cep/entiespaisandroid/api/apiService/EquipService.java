ad026e1c12b5affae6d26c50c44fc2f276b97ab5
superclevver <44167881+superclevver@users.noreply.github.com>
2019-04-01 10:53:32 +0100

    Update EquipService.java

--- app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/EquipService.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/EquipService.java
@@ -8,6 +8,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.DELETE;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Body;
 
 public interface EquipService
 {
