3fe20659c04af754fb5ba14378ffc7dcf8bf68c1
superclevver <44167881+superclevver@users.noreply.github.com>
2019-04-01 10:58:17 +0100

    Update AdministradorService.java

--- app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/AdministradorService.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/AdministradorService.java
@@ -6,11 +6,24 @@ import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.DELETE;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Body;
 
 public interface AdministradorService
 {
 	@GET("api/ADMINISTRADORS")
 	Call<ArrayList<ADMINISTRADORS>> getAdministradors();
-
-	//..
+	
+	@POST("api/ADMINISTRADORS")
+	Call<ADMINISTRADORS> insertAdministrador(@Body ADMINISTRADORS administrador);
+	
+	@DELETE("api/ADMINISTRADORS/{id}")
+	Call<ADMINISTRADORS> deleteAdministrador(@Path("id") int id);
+		
+ 	@PUT("api/ADMINISTRADORS/{id}")
+	Call<ADMINISTRADORS> modificarAdministrador(@Path("id") int id, @Body ADMINISTRADORS administrador);
+	
 }
