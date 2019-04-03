0141931698489f6dcf06efd33300354c1221a819
JoelGalimany <joel.galimany.13@gmail.com>
2019-04-03 10:19:02 +0200

    API

--- app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/Demanda_ActService.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/Demanda_ActService.java
@@ -5,10 +5,24 @@ import com.example.cep.entiespaisandroid.classes.DEMANDA_ACT;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface Demanda_ActService
 {
 	@GET("api/DEMANDA_ACT")
 	Call<ArrayList<DEMANDA_ACT>> getDemanda_acts();
+
+	@GET("api/DEMANDA_ACT/{id}")
+	Call<DEMANDA_ACT> getDemanda_actId(@Path("id")int id);
+
+	@POST("api/DEMANDA_ACT")
+	Call<DEMANDA_ACT> InsertDemanda_act(@Body DEMANDA_ACT demanda_act);
+
+	@DELETE("api/DEMANDA_ACT")
+	Call<DEMANDA_ACT> DeleteDemandaAct(@Body DEMANDA_ACT demanda_act);
+
 }
--- app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/HoresService.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/api/apiService/HoresService.java
@@ -1,14 +1,32 @@
 package com.example.cep.entiespaisandroid.api.apiService;
 
+import com.example.cep.entiespaisandroid.classes.HORARI_ACTIVITAT;
 import com.example.cep.entiespaisandroid.classes.HORES;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface HoresService
 {
 	@GET("api/HORES")
 	Call<ArrayList<HORES>> getHores();
+
+	@GET("api/HORES/{id}")
+	Call<HORES> getHoresId(@Path("id")int id);
+
+	@POST("api/HORES")
+	Call<HORES> InsertHores(@Body HORES hores);
+
+	@DELETE("api/HORES")
+	Call<HORES> DeleteHores(@Body HORES hores);
+
+	@PUT("api/HORES")
+	Call<HORES> UpdateHores(@Body HORES hores);
 }
