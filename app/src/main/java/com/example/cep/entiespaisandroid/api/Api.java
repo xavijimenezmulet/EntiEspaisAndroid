30ed0c8e27fa272ca18d6f01787cee09cd1af037
cep <rwtgrertr3>
2019-04-08 10:56:59 +0200

    update

--- app/src/main/java/com/example/cep/entiespaisandroid/api/Api.java
+++ app/src/main/java/com/example/cep/entiespaisandroid/api/Api.java
@@ -14,7 +14,7 @@ public class Api {
 	public static final String IP_PC_CLASSE_DAVID   = "";
 	public static final String IP_PC_CASA_DAVID   = "";
 
-	private static final String BASE_URL = "http://10.0.2.2:53808/";
+	private static final String BASE_URL = /*"http://10.0.2.2:53808/"*/ "http://172.16.110.1:53808/";
 	private static Retrofit  retrofit = null;
 
 	public static Retrofit getApi(){
